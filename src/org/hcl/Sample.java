18bcf7895b30b73d1f74fba53af6b134c5c5a5ae
YBenistan <benistan.0505@gmail.com>
2023-01-31 13:07:43 +0530

    testing

---
+++ src/org/hcl/Sample.java
@@ -0,0 +1,30 @@
+package org.hcl;
+
+import java.util.Scanner;
+
+public class Sample {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		System.out.println("Enter the number A :"+a);
+		int b = s.nextInt();
+		System.out.println("Enter the number B :"+b);
+		int c = s.nextInt();
+		System.out.println("Enter the number C :"+c);
+		//int a = 10,b=20,c=30;
+		if(a>b&&a>c)
+		{
+			System.out.println("A is greater :"+a);
+		}
+		else if (b>a&&b>c) {
+			System.out.println("B is greater :"+b);
+			
+		}
+		else
+		{
+			System.out.println("C is greater :"+c);
+		}
+		
+	}
+
+}
